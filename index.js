bd13aea4c32de916a8ff32c910733bbf3f0811d7
LindseyIsaac <isaaclindsey89@gmail.com>
2023-05-03 13:04:01 -0500

    im lost man

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const { Shape, Triangle, Square, Circle } = require('./lib/shapes.js');
+//does order matter here
+const { Shape, Circle, Triangle, Square } = require("./lib/shapes.js");
 const fs = require("fs");
-const path = require("path");
 const inquirer = require("inquirer");
+const path = require("path");
 // question for initials aka letters for logo
 inquirer.prompt ([
     {
-        type: "checkbox",
+        type: "list",
         name: "userShape",
         message: "Select your shape",
-        choices: ["Triangle", "Square", "Circle"],
+        choices: ["Circle", "Triangle", "Square"]
         },
     {
         type: "input",
@@ -17,12 +18,12 @@ inquirer.prompt ([
         },
     {
         type: "input",
-        name: "initText",
+        name: "Text",
         message: "Make a combo of 3 letters please, no more than 3!",
         },
     {
         type: "input",
-        name: "initTextColor",
+        name: "textColor",
         message: "This colors the initials text.",
         },   
 
@@ -33,33 +34,37 @@ inquirer.prompt ([
 // ? for letters color
 
 .then((answers) => {
-    let userShape= answers.shape ;
-    let userColor = answers.color;
-    let initText = answers.text;
-    let initTextColor = answers.textColor;
+    let userShape= answers.userShape;
+    let userColor = answers.userColor;
+    let Text = answers.Text;
+    let textColor = answers.textColor;
     
-    if (userShape == "Triangle") {
-        const triangle =  new Triangle(userColor, initText, initTextColor);
-        const output = triangle.makeTriangle(triangle.color, triangle.text, triangle.textColor);
+    if (userShape == "Circle") {
+        const circle = new Circle(userColor, Text, textColor);
+        const output = circle.makeCircle(Circle.color, Circle.text, Circle.textColor);
             console.log(output);
-    fs.writeFile(`../examples/tri-${newText}.svg`, output, (err) =>
+
+    fs.writeFile(`/examples/cir-${Text}.svg`, output, (err) =>
+        err ? console.log(err) : console.log('Circle successfully made!')
+        );
+    }
+    else if (userShape == "Triangle") {
+        const triangle = new Triangle(userColor, Text, textColor);
+        const output = Triangle.makeTriangle(triangle.color, triangle.Text, triangle.textColor);
+            console.log(output);
+
+    fs.writeFile(`/examples/tri-${Text}.svg`, output, (err) =>
         err ? console.log(err) : console.log('Triangle made successfully!')
         );
     }
     else if (userShape == "Square") {
-        const square =  new Square(userColor, initText, initTextColor);
-        const output = square.makeSquare(square.color, square.text, square.textColor);
+        const square = new Square(userColor, Text, textColor);
+        const output = square.makeSquare(square.color, square.Text, square.textColor);
         console.log(output);
-    fs.writeFile(`../examples/sq-${newText}.svg`, output, (err) =>
+
+    fs.writeFile(`/examples/sq-${Text}.svg`, output, (err) =>
         err ? console.log(err) : console.log('Square successfully made!')
         );
     }
-    else if (userShape == "Circle") {
-        const circle =  new Circle(userColor, initText, initTextColor);
-        const output = circle.makeCircle(circle.color, circle.text, circle.textColor);
-            console.log(output);
-    fs.writeFile(`../examples/cir-${newText}.svg`, output, (err) =>
-        err ? console.log(err) : console.log('Circle successfully made!')
-        );
-    }
+
 });
