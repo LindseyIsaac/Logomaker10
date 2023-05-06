124635fb50edb0b07cd48ba482c90332a5a82df6
LindseyIsaac <isaaclindsey89@gmail.com>
2023-05-05 19:18:32 -0500

    ha got it

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ inquirer.prompt ([
         },
     {
         type: "input",
-        name: "Text",
+        name: "text",
         message: "Make a combo of 3 letters please, no more than 3!",
         },
     {
@@ -36,33 +36,33 @@ inquirer.prompt ([
 .then((answers) => {
     let userShape= answers.userShape;
     let userColor = answers.userColor;
-    let Text = answers.Text;
+    let text = answers.text;
     let textColor = answers.textColor;
     
     if (userShape == "Circle") {
-        const circle = new Circle(userColor, Text, textColor);
-        const output = circle.makeCircle(Circle.color, Circle.text, Circle.textColor);
+        const circle = new Circle(userColor, text, textColor);
+        const output = circle.makeCircle(circle.color, circle.text, circle.textColor);
             console.log(output);
 
-    fs.writeFile(`/examples/cir-${Text}.svg`, output, (err) =>
+    fs.writeFile(`examples/cir-${text}.svg`, output, (err) =>
         err ? console.log(err) : console.log('Circle successfully made!')
         );
     }
     else if (userShape == "Triangle") {
-        const triangle = new Triangle(userColor, Text, textColor);
-        const output = Triangle.makeTriangle(triangle.color, triangle.Text, triangle.textColor);
+        const triangle = new Triangle(userColor, text, textColor);
+        const output = triangle.makeTriangle(triangle.color, triangle.text, triangle.textColor);
             console.log(output);
 
-    fs.writeFile(`/examples/tri-${Text}.svg`, output, (err) =>
+    fs.writeFile(`examples/tri-${text}.svg`, output, (err) =>
         err ? console.log(err) : console.log('Triangle made successfully!')
         );
     }
     else if (userShape == "Square") {
-        const square = new Square(userColor, Text, textColor);
-        const output = square.makeSquare(square.color, square.Text, square.textColor);
+        const square = new Square(userColor, text, textColor);
+        const output = square.makeSquare(square.color, square.text, square.textColor);
         console.log(output);
 
-    fs.writeFile(`/examples/sq-${Text}.svg`, output, (err) =>
+    fs.writeFile(`examples/sq-${text}.svg`, output, (err) =>
         err ? console.log(err) : console.log('Square successfully made!')
         );
     }
